2b5e47c0622c418dfc9a8fd7869288f1d6d63f28
Arthur Lu <learthurgo@gmail.com>
2022-10-24 18:30:22 -0700

    implement expose, add on change listener for horn selection, add on change listener for volume slider, add on click listener for horn playing
    Signed-off-by: Arthur Lu <learthurgo@gmail.com>

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,48 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let horn_select = document.getElementById("horn-select");
+  horn_select.addEventListener('change', function(){
+    let index = horn_select.selectedIndex;
+    let files = ["air-horn", "car-horn", "party-horn"];
+
+    let image = document.querySelector("#expose img");
+    let prefix = "assets/images/";
+    let suffix = ".svg"
+    image.src = prefix + files[index - 1] + suffix;
+
+    let audio = document.querySelector("audio");
+    prefix = "assets/audio/";
+    suffix = ".mp3";
+    audio.src = prefix + files[index - 1] + suffix;
+  });
+
+  let volume_slider = document.getElementById("volume");
+  volume_slider.addEventListener('change', function(){
+    let volume = volume_slider.value;
+    let image = document.querySelector("#volume-controls img");
+
+    let files = ["volume-level-0", "volume-level-1", "volume-level-2", "volume-level-3"];
+    let prefix = "assets/icons/";
+    let suffix = ".svg"
+
+    if (volume < 1) {image.src = prefix + files[0] + suffix;}
+    else if (volume < 33) {image.src = prefix + files[1] + suffix;}
+    else if (volume < 67) {image.src = prefix + files[2] + suffix;}
+    else {image.src = prefix + files[3] + suffix;}
+
+    let audio = document.querySelector("audio");
+    audio.volume = volume / 100;
+  });
+
+  let play_button = document.querySelector("button");
+  play_button.addEventListener('click', function(){
+    let audio = document.querySelector("audio");
+    audio.play();
+    let index = horn_select.selectedIndex;
+    if (index === 3) {
+      const jsConfetti = new JSConfetti()
+      jsConfetti.addConfetti()
+    }
+  });
 }
