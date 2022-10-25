accb7bce4dfdbd9416af4a6f1928561cfe03f9c3
Arthur Lu <learthurgo@gmail.com>
2022-10-24 18:54:13 -0700

    move common elements outside functions as const, move common filenames outside functions as const
    Signed-off-by: Arthur Lu <learthurgo@gmail.com>

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,53 +2,53 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
-  let horn_select = document.getElementById("horn-select");
-  horn_select.addEventListener('change', handleHornSelect);
+const horn_select = document.getElementById("horn-select");
+const volume_slider = document.getElementById("volume");
+const play_button = document.querySelector("button");
 
-  let volume_slider = document.getElementById("volume");
+const horn_files = ["air-horn", "car-horn", "party-horn"];
+const volume_files = ["volume-level-0", "volume-level-1", "volume-level-2", "volume-level-3"];
+
+function init() {
+  
+  horn_select.addEventListener('change', handleHornSelect);
+ 
   volume_slider.addEventListener('change', handleVolumeSlider);
 
-  let play_button = document.querySelector("button");
   play_button.addEventListener('click', handlePlayButton);
 }
 
 function handleHornSelect () {
-  let horn_select = document.getElementById("horn-select");
   let index = horn_select.selectedIndex;
-  let files = ["air-horn", "car-horn", "party-horn"];
-
+  
   let image = document.querySelector("#expose img");
   let prefix = "assets/images/";
   let suffix = ".svg"
-  image.src = prefix + files[index - 1] + suffix;
+  image.src = prefix + horn_files[index - 1] + suffix;
 
   let audio = document.querySelector("audio");
   prefix = "assets/audio/";
   suffix = ".mp3";
-  audio.src = prefix + files[index - 1] + suffix;
+  audio.src = prefix + horn_files[index - 1] + suffix;
 }
 
 function handleVolumeSlider () {
-  let volume_slider = document.getElementById("volume");
   let volume = volume_slider.value;
   let image = document.querySelector("#volume-controls img");
-
-  let files = ["volume-level-0", "volume-level-1", "volume-level-2", "volume-level-3"];
+  
   let prefix = "assets/icons/";
   let suffix = ".svg"
 
-  if (volume < 1) {image.src = prefix + files[0] + suffix;}
-  else if (volume < 33) {image.src = prefix + files[1] + suffix;}
-  else if (volume < 67) {image.src = prefix + files[2] + suffix;}
-  else {image.src = prefix + files[3] + suffix;}
+  if (volume < 1) {image.src = prefix + volume_files[0] + suffix;}
+  else if (volume < 33) {image.src = prefix + volume_files[1] + suffix;}
+  else if (volume < 67) {image.src = prefix + volume_files[2] + suffix;}
+  else {image.src = prefix + volume_files[3] + suffix;}
 
   let audio = document.querySelector("audio");
   audio.volume = volume / 100;
 }
 
 function handlePlayButton () {
-  let horn_select = document.getElementById("horn-select");
   let audio = document.querySelector("audio");
   audio.play();
   let index = horn_select.selectedIndex;
